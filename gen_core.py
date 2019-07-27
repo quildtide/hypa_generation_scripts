15cc3ca1f5d92166e8e58def6578ad0a0cc772bb
Thalassocracy <42811940+Thalassocracy@users.noreply.github.com>
2019-07-26 20:04:35 -0500

    Hypa generator script kinda done

---
+++ gen_core.py
@@ -0,0 +1,35 @@
+import json
+
+base_directory = 'F:/SteamSSD/steamapps/common/Planetary Annihilation Titans/media'
+
+with open('unit_list.json') as unit_list:
+    units = json.load(unit_list)['units']
+
+for i in range(len(units)):
+    try:
+        titans = '/pa_ex1' + units[i][3:]
+        with open(base_directory + titans) as t:
+            units[i] = titans
+    except:
+        c = 0
+
+
+tools = []
+
+for i in units:
+    filename = base_directory + i
+    with open(filename) as unitfile:
+        unit = json.load(unitfile)
+        try:
+            for j in unit['tools']:
+                tools.append(j['spec_id'])
+        except:
+            c = 0
+
+for i in range(len(tools)):
+    try:
+        titans = '/pa_ex1' + tools[i][3:]
+        with open(base_directory + titans) as t:
+            tools[i] = titans
+    except:
+        c = 0
---
+++ gen_hypa.1.py
@@ -0,0 +1,74 @@
+import gen_core
+import json
+import os
+
+units = gen_core.units
+
+tools = gen_core.tools
+
+base_directory = gen_core.base_directory
+
+for i in units:
+    with open(base_directory + i) as k:
+        unit = json.load(k)
+        if "production" in unit:
+            if "metal" in unit["production"]:
+                unit["production"]["metal"] = unit["production"]["metal"] * 2
+        if "storage" in unit:
+            if "metal" in unit["storage"]:
+                unit["storage"]["metal"] = unit["storage"]["metal"] * 2
+
+        if "factory_cooldown_time" in unit:
+            unit["factory_cooldown_time"] = unit["factory_cooldown_time"] // 2
+
+        if "navigation" in unit:
+            if "move_speed" in unit["navigation"]:
+                unit["navigation"]["move_speed"] = int(unit["navigation"]["move_speed"] * 1.5)
+            if "acceleration" in unit["navigation"]:
+                unit["navigation"]["acceleration"] = int(unit["navigation"]["acceleration"] * 1.5)
+            if "turn_speed" in unit["navigation"]:
+                unit["navigation"]["turn_speed"] = int(unit["navigation"]["turn_speed"] * 1.5)
+
+
+        if i[0:7] == '/pa_ex1':
+            i = '/pa' + i[7:] 
+
+        try:
+            with open('hypa' + i, 'w+') as out:
+                c = 0
+        except:
+            os.makedirs("/".join(('hypa' + i).split("/")[:-1]))
+
+        with open('hypa' + i, 'w+') as out:
+            json.dump(unit, out)
+
+for i in tools:
+    with open(base_directory + i) as k:
+        tool = json.load(k)
+
+        if "construction_demand" in tool:
+            if "metal" in tool["construction_demand"]:
+                tool["construction_demand"]["metal"] = tool["construction_demand"]["metal"] * 2
+
+        if "rate_of_fire" in tool:
+            tool["rate_of_fire"] = tool["rate_of_fire"] * 1.5
+        if "pitch_rate" in tool:
+            tool["pitch_rate"] = tool["pitch_rate"] * 1.5
+        if "yaw_rate" in tool:
+            tool["yaw_rate"] = tool["yaw_rate"] * 1.5
+
+        if "ammo_demand" in tool:
+            tool["ammo_demand"] = tool["ammo_demand"] * 1.5     
+
+
+        if i[0:7] == '/pa_ex1':
+            i = '/pa' + i[7:] 
+
+        try:
+            with open('hypa' + i, 'w+') as out:
+                c = 0
+        except:
+            os.makedirs("/".join(('hypa' + i).split("/")[:-1]))
+
+        with open('hypa' + i, 'w+') as out:
+            json.dump(tool, out)
