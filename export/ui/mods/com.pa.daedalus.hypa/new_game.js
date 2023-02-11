6c7cceca287227934534581ab11a17e0c3f56143
quildtide <42811940+quildtide@users.noreply.github.com>
2023-02-11 14:21:32 -0500

    Update modinfos for v1.1.4

--- export/ui/mods/com.pa.daedalus.hypa/new_game.js
+++ export/ui/mods/com.pa.daedalus.hypa/new_game.js
@@ -1,5 +1,5 @@
-model.localChatMessage(loc("!LOC:HyPA","!LOC:!!! WARNING !!!"));
-model.localChatMessage(loc("!LOC:HyPA","!LOC:2.0x Build Speed (and Metal Income)!"));
-model.localChatMessage(loc("!LOC:HyPA","!LOC:1.5x Attack and Move Speed!"));
-model.localChatMessage(loc("!LOC:HyPA","!LOC:2x Commander HP."));
-model.localChatMessage(loc("!LOC:HyPA","!LOC:Be prepared!"));
+model.localChatMessage(loc("!LOC:HyPA"),loc("!LOC:!!! WARNING !!!"));
+model.localChatMessage(loc("!LOC:HyPA"),loc("!LOC:2.0x Build Speed (and Metal Income)!"));
+model.localChatMessage(loc("!LOC:HyPA"),loc("!LOC:1.5x Attack and Move Speed!"));
+model.localChatMessage(loc("!LOC:HyPA"),loc("!LOC:2x Commander HP."));
+model.localChatMessage(loc("!LOC:HyPA"),loc("!LOC:Be prepared!"));
