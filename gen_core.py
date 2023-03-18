7c3f8a0e04ca8123e015aeb062da64e4fe7511c7
quildtide <42811940+quildtide@users.noreply.github.com>
2023-03-18 04:27:03 -0400

    Disable Thorosmen support for now

--- gen_core.py
+++ gen_core.py
@@ -17,7 +17,7 @@ mod_urls = {
     "dozer": "https://github.com/DAEDALUS-Modding/Dozer/archive/release.zip",
     "bugs": "https://github.com/Ferret-Master/Bug-Faction/archive/refs/heads/main.zip",
     # "scenario_units": "https://github.com/Ferret-Master/Scenario-Server/archive/refs/heads/release.zip",
-    "thorosmen": "https://github.com/ATLASLORD/Thorosmen/archive/refs/heads/main.zip",
+    # "thorosmen": "https://github.com/ATLASLORD/Thorosmen/archive/refs/heads/main.zip",
     "upgradable_turrets": "https://github.com/BotWhan/com.pa.upgradable-turrets/archive/main.zip"
 }
 
