e73e4cb7993103b086f9ca35d87bc845d5ee6eca
quildtide <42811940+quildtide@users.noreply.github.com>
2023-04-15 20:03:39 -0400

    set dev mod_id suffix in install_mods.py

--- install_mods.py
+++ install_mods.py
@@ -1,5 +1,6 @@
 import shutil
 import os
+import json
 
 from pa_directory import mods_path
 
@@ -9,5 +10,13 @@ def install_mod(mod_path):
         shutil.rmtree(install_path)
 
     shutil.copytree(mod_path, install_path, dirs_exist_ok=True)
+
+    modinfo_path = os.path.join(install_path, "modinfo.json")
+    with open(modinfo_path, 'r') as modinfo_file:
+        info = json.load(modinfo_file)
+        info["identifier"] = info["identifier"] + "-dev"
+    
+    with open(modinfo_path, 'w') as modinfo_file:
+        json.dump(info, modinfo_file)
 
 install_mod("hypa")
